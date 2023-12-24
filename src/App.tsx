bd885580a77185365b322666264b00a4d4b0a6b5
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-12-24 22:45:35 +0545

    add expense tracker form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import ExpenseForm from "./components/ExpenseForm";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <h1>Hello World</h1>;
+  return <ExpenseForm />;
 }
 
 export default App;
---
+++ src/components/ExpenseForm.tsx
@@ -0,0 +1,92 @@
+import React, { FormEvent, useRef, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  description: z.string().min(1, { message: "Description is required." }),
+  amount: z
+    .number({ invalid_type_error: "Amount field is required." })
+    .min(0, { message: "Amount must be a positive number." }),
+  category: z.string().min(1, { message: "Category is required." }),
+});
+
+type FormData = z.infer<typeof schema>;
+
+const ExpenseForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <h2>My Expense Tracker</h2>
+      <hr />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-3">
+          <label htmlFor="description" className="form-label">
+            Description
+          </label>
+          <input
+            {...register("description")}
+            id="description"
+            type="text"
+            className="form-control"
+          />
+          {errors.description && (
+            <p className="text-danger">{errors.description.message}</p>
+          )}
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="amount" className="form-label">
+            Amount
+          </label>
+          <input
+            {...register("amount", { valueAsNumber: true })}
+            id="amount"
+            type="number"
+            className="form-control"
+          />
+
+          {errors.amount && (
+            <p className="text-danger">{errors.amount.message}</p>
+          )}
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="category" className="form-label">
+            Category
+          </label>
+          <select
+            {...register("category")}
+            id="category"
+            name="category"
+            className="form-control"
+          >
+            <option value="All Categories">All Categories</option>
+            <option value="Groceries">Groceries</option>
+            <option value="Utilities">Utilities</option>
+            <option value="Entertainment">Entertainment</option>
+          </select>
+
+          {errors.category && (
+            <p className="text-danger">{errors.category.message}</p>
+          )}
+        </div>
+
+        <button className="btn btn-primary" type="submit">
+          Submit
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default ExpenseForm;
